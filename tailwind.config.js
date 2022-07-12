ba9e4548938cf1a53654351eb408d9cbb7665504
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-07-12 23:12:32 +0700

    update lại môi trường

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
